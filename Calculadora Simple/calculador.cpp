ac0ae59239698b54f59aed985d26e71e2936cae0
Govea <diego.govea4992@alumnos.udg.mx>
2024-08-27 11:24:51 -0600

    Ola

---
+++ Calculadora Simple/calculador.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Aquí puedes escribir tu código
+    for (int i = 0; i < 10; i++) {
+        cout << "Contador: " << i << endl;
+    }
+
+    return 0;
+}
